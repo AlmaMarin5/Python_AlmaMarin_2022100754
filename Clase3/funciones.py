7994a3db133edbb838e6b733216fe95094f95c28
alma <almamarin003@gmail.com>
2025-04-25 16:55:21 -0300

    Clase 3

---
+++ Clase3/funciones.py
@@ -0,0 +1,23 @@
+def sumar(a, b):
+    return a + b
+
+#ejemplo de uso
+resultado = sumar(3,5)
+print(f"la suma es:{resultado}")
+
+def resto(a,b):
+    return a%b
+
+n1=3
+n2=5
+resultado=0
+resultado=sumar(n1,n2)
+print(f"la suma es:{resultado}")
+resultado=resta(n1,n2)
+print(f"la resta es:{resultado}")
+resultado=mult(n1,n2)
+print(f"la mult es:{resultado}")
+resultado=div(n1,n2)
+print(f"la div es:{resultado}")
+resultado=resto(n1,n2)
+print(f"el resto es:{resultado}")
---
+++ Clase3/while.py
@@ -0,0 +1,7 @@
+#while - mientras que
+
+numero=0
+
+while numero < 20:
+    print(numero)
+    numero +=1
